324f2fa56caf4b40fb6a427392205991157e55a0
Dennis Lund <Dennis.l.christiansen@gmail.com>
2015-05-09 03:51:16 +0200

    Kodet første udkast af BilAccess og SælgerAccess (Attributten "Pris" i BilAccess skal formentligt ændres til typen "Big-Decimal").

--- 04-Implemetation/Ferrari Finansiering/src/dataAccess/BilAccess.java
+++ 04-Implemetation/Ferrari Finansiering/src/dataAccess/BilAccess.java
@@ -1,7 +1,6 @@
 package dataAccess;
 
 import java.sql.*;
-import java.util.*;
 
 import domain.Bil;
 
@@ -10,7 +9,7 @@ public class BilAccess
 {
   private static final String SELECT = "SELECT registreringsnummer, pris, model, årgang, bemærkninger FROM bil WHERE chassisnummer = ?";
 //  private static final String SELECT_MANY = "SELECT registreringsnummer, pris, model, årgang, bemærkninger FROM bil WHERE ";
-  private static final String INSERT = "INSERT INTO bil(registreringsnummer, pris, model, årgang, bemærkninger) VALUES (?, ?, ?, ?, ?)";
+  private static final String INSERT = "INSERT INTO bil(chassisnummer, registreringsnummer, pris, model, årgang, cprid, bemærkninger) VALUES (?, ?, ?, ?, ?, ?, ?)";
   private static final String UPDATE = "UPDATE bil SET registreringsnummer = ?, pris = ?, model = ?, årgang = ?, bemærkninger = ? WHERE chassisnummer = ?";
   private static final String DELETE = "DELETE FROM bil WHERE chassisnummer = ?";
   
@@ -24,24 +23,201 @@ public class BilAccess
   /*
    * Create
    */
+  public void createBil(Bil bil) throws SQLException
+  {
+    Connection connection = null;
+    try
+    {
+      connection = new DbConnection().getConnection();
+      createBil( connection, bil );
+    }
+    finally
+    {
+      if ( connection != null )
+      {
+        connection.close();
+      }
+    }
+  }
   
+  public void createBil(Connection connection, Bil bil) throws SQLException
+  {
+    PreparedStatement statement = null;
+//    ResultSet resultset = null;
+    try
+    {
+      statement = connection.prepareStatement( INSERT );
+      statement.setString( 1, bil.getChassisnummer() );
+      statement.setString( 2, bil.getRegistreringsnummer() );
+      statement.setDouble( 3, bil.getPris() );
+      statement.setString( 4, bil.getModel() );
+      statement.setString( 5, bil.getÅrgang() );
+      statement.setInt( 6, bil.getCprid() );
+      statement.setString( 7, bil.getBemærkninger() );
+      statement.execute();
+      connection.commit();
+    }
+    finally
+    {
+//      if ( resultset != null )
+//      {
+//        resultset.close();
+//        System.out.println("Bil, resultset close");
+//      }
+      if ( statement != null )
+      {
+        statement.close();
+//        System.out.println("Bil, statement close");
+      }
+    }
+    
+  }
   
   
   /*
    * Read
    */
+  public Bil readBil(String chassisnummer) throws SQLException
+  {
+    Connection connection = null;
+    try
+    {
+      connection = new DbConnection().getConnection();
+      return readBil( connection, chassisnummer );
+    }
+    finally
+    {
+      if ( connection != null )
+      {
+        connection.close();
+      }
+    }
+  }
   
+  public Bil readBil(Connection connection, String chassisnummer) throws SQLException
+  {
+    PreparedStatement statement = null;
+    ResultSet resultset = null;
+    Bil bil = null;
+    try
+    {
+      statement = connection.prepareStatement( SELECT );
+      statement.setString( 1, chassisnummer );
+      resultset = statement.executeQuery();
+      bil = new Bil();
+      while ( resultset.next() )
+      {
+      bil.setRegistreringsnummer( resultset.getString( "registreringsnummer" ) );
+      bil.setPris( resultset.getDouble( "pris" ) );
+      bil.setModel( resultset.getString( "model" ) );
+      bil.setÅrgang( resultset.getString( "årgang" ) );
+      bil.setBemærkninger( resultset.getString( "bemærkninger" ) );
+      }
+      return bil;
+    }
+    finally
+    {
+      if ( resultset != null )
+      {
+        resultset.close();
+      }
+      if ( statement != null )
+      {
+        statement.close();
+      }
+    }
+    
+    
+  }
   
   
   /*
    * Update
    */
+  public void updateBil(Bil bil) throws SQLException
+  {
+    Connection connection = null;
+    try
+    {
+      connection = new DbConnection().getConnection();
+      updateBil( connection, bil );
+    }
+    finally
+    {
+      if ( connection != null )
+      {
+        connection.close();
+      }
+    }
+  }
+  
+
+  
+  public void updateBil(Connection connection, Bil bil) throws SQLException
+  {
+    PreparedStatement statement = null;
+    try
+    {
+      statement = connection.prepareStatement( UPDATE );
+      statement.setString( 1, bil.getChassisnummer() );
+      statement.setString( 2, bil.getRegistreringsnummer() );
+      statement.setDouble( 3, bil.getPris() );
+      statement.setString( 4, bil.getModel() );
+      statement.setString( 5, bil.getÅrgang() );
+      statement.setInt( 6, bil.getCprid() );
+      statement.setString( 7, bil.getBemærkninger() );
+      statement.execute();
+      connection.commit();
+    }
+    finally
+    {
+      if ( statement != null )
+      {
+        statement.close();
+      }
+    }
+    
+    
+  }
   
   
-  
-  /*
-   * Delete
-   */
+    /*
+     * Delete
+     */  
+    public void deleteBil(String chassisnummer) throws SQLException
+    {
+      Connection connection = null;
+      try
+      {
+        connection = new DbConnection().getConnection();
+        deleteBil( connection, chassisnummer );
+      }
+      finally
+      {
+        if ( connection != null )
+        {
+          connection.close();
+        }
+      }
+    }
+    
+    public void deleteBil(Connection connection, String chassisnummer) throws SQLException
+    {
+      PreparedStatement statement = null;
+      try
+      {
+        statement = connection.prepareStatement( DELETE );
+        statement.setString( 1, chassisnummer );
+        statement.execute();
+      }
+      finally
+      {
+        if ( statement != null )
+        {
+          statement.close();
+        }
+      }
+    }
   
   
   
--- 04-Implemetation/Ferrari Finansiering/src/dataAccess/SælgerAccess.java
+++ 04-Implemetation/Ferrari Finansiering/src/dataAccess/SælgerAccess.java
@@ -1,7 +1,6 @@
 package dataAccess;
 
 import java.sql.*;
-import java.util.*;
 
 import domain.Sælger;
 
@@ -9,7 +8,7 @@ import domain.Sælger;
 public class SælgerAccess
 {
   private static final String SELECT = "SELECT navn FROM sælger WHERE sælgerid = ?";
-//  private static final String SELECT_MANY = "SELECT  FROM  WHERE ";
+  //  private static final String SELECT_MANY = "SELECT  FROM  WHERE ";
   private static final String INSERT = "INSERT INTO sælger(sælgernavn) VALUES (?)";
   private static final String UPDATE = "UPDATE sælger SET sælgernavn WHERE sælgerid = ?";
   private static final String DELETE = "DELETE FROM sælger WHERE sælgerid = ?";
@@ -24,25 +23,164 @@ public class SælgerAccess
   /*
    * Create
    */
+  public void createSælger( Sælger sælger ) throws SQLException
+  {
+    Connection connection = null;
+    try
+    {
+      connection = new DbConnection().getConnection();
+      createSælgernavn( connection, sælger );
+    }
+    finally
+    {
+      if ( connection != null )
+      {
+        connection.close();
+        //        System.out.println("sælger, connection close");
+      }
+    }
+  }
   
-  
+  public void createSælgernavn( Connection connection, Sælger sælger ) throws SQLException
+  {
+    PreparedStatement statement = null;
+    try
+    {
+      statement = connection.prepareStatement( INSERT ); 
+      statement.setString( 1, sælger.getSælgernavn() );
+      statement.execute();
+    }
+    finally
+    {
+      if ( statement != null )
+      {
+        statement.close();
+        //        System.out.println("sælger, statement close");
+      }
+    }
+  }
   
   /*
    * Read
    */
+  public Sælger readSælgernavn( int sælgerid ) throws SQLException
+  {
+    Connection connection = null;
+    try
+    {
+      connection = new DbConnection().getConnection();
+      return readSælgernavn( connection, sælgerid );
+    }
+    finally
+    {
+      if ( connection != null )
+      {
+        connection.close();
+      }
+    }
+  }
+  
+  public Sælger readSælgernavn( Connection connection, int sælgerid ) throws SQLException
+  {
+    PreparedStatement statement = null;
+    ResultSet resultset = null;
+    Sælger sælger = null;
+    try
+    {
+      statement = connection.prepareStatement( SELECT );
+      statement.setInt( 1, sælgerid );
+      resultset = statement.executeQuery();
+      sælger = new Sælger();
+      sælger.setSælgerid( sælgerid );
+      sælger.setSælgernavn( resultset.getString( "sælgernavn" ) );
+      return sælger;
+    }
+    finally
+    {
+      
+    }
+  }
   
   
   
   /*
    * Update
    */
+      
+  public void updateSælgernavn( Sælger sælger ) throws SQLException
+  {
+    Connection connection = null;
+    try
+    {
+      connection = new DbConnection().getConnection();
+      updateSælgernavn( connection, sælger );
+    }
+    finally
+    {
+      if ( connection != null )
+      {
+        connection.close();
+      }
+    }
+  }
   
+  public void updateSælgernavn( Connection connection, Sælger sælger ) throws SQLException
+  {
+    PreparedStatement statement = null;
+    try
+    {
+      statement = connection.prepareStatement( UPDATE );
+      statement.setString( 1, sælger.getSælgernavn() );
+      statement.execute();
+      
+    }
+    finally
+    {
+      if ( statement != null )
+      {
+        statement.close();
+      }
+    }
+  }
   
   
   /*
    * Delete
    */
+  public void deleteSælger( int sælgerid ) throws SQLException
+  {
+    Connection connection = null;
+    try
+    {
+      connection = new DbConnection().getConnection();
+      deleteSælger( connection, sælgerid );
+    }
+    finally
+    {
+      if ( connection != null )
+      {
+        connection.close();
+      }
+    }
+  }
   
+  public void deleteSælger( Connection connection, int sælgerid ) throws SQLException
+  {
+    PreparedStatement statement = null;
+    try
+    {
+      statement = connection.prepareStatement( DELETE );
+      statement.setInt( 1, sælgerid );
+      statement.execute();
+    }
+    finally
+    {
+      if ( statement != null )
+      {
+        statement.close();
+      }
+    }
+  }
   
   
 }
