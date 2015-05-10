397d73abbeb0b6f54296dad55a297f2859692148
Dennis Lund <Dennis.l.christiansen@gmail.com>
2015-05-10 23:21:56 +0200

    Moved MainWindow to prototype-package (Where it belongs).

--- 04-Implemetation/Ferrari Finansiering/src/prototypes/MainWindow.java
+++ 04-Implemetation/Ferrari Finansiering/src/prototypes/MainWindow.java
@@ -1,14 +1,11 @@
-package presentation;
+package prototypes;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
+import java.awt.*;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.border.*;
 
 import logic.*;
-import java.awt.Dimension;
 
 
 
@@ -16,6 +13,7 @@ public class MainWindow extends JFrame
 {
   
   private JPanel contentPane;
+  private JTextField searchField;
   
 
   
@@ -26,7 +24,7 @@ public class MainWindow extends JFrame
   {
     setBackground(Color.RED);
     setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    setBounds( 100, 100, 300, 400 );
+    setBounds( 100, 100, 500, 500 );
     contentPane = new JPanel();
     contentPane.setBorder( new EmptyBorder( 10, 10, 10, 10 ) );
     setContentPane( contentPane );
@@ -36,9 +34,12 @@ public class MainWindow extends JFrame
     JPanel logopanel = LogoPanel();
     contentPane.add(logopanel, BorderLayout.NORTH);
     
-    JPanel sælgerpanel = SælgerPanel();
-    contentPane.add(sælgerpanel, BorderLayout.CENTER);
+//    JPanel sælgerpanel = SælgerPanel();
+//    contentPane.add(sælgerpanel, BorderLayout.CENTER);
     
+    JPanel findkundepanel = FindKundePanel();
+    contentPane.add(findkundepanel, BorderLayout.CENTER);
+
     
     
   }
@@ -92,8 +93,9 @@ public class MainWindow extends JFrame
   private JPanel LogoPanel()
   {
     JPanel logopanel = new JPanel();
+    FlowLayout flowLayout = (FlowLayout) logopanel.getLayout();
+    flowLayout.setAlignment(FlowLayout.LEFT);
     logopanel.setBackground(Color.RED);
-    
     JLabel label = new JLabel(new ImageIcon("C:\\Users\\Dennis\\Documents\\GitHub\\1YearProject-Gruppe-3\\04-Implemetation\\Ferrari Finansiering\\Yellow logo on red background.png"));
     label.setLabelFor(label);
     label.setSize(new Dimension(300, 100));
@@ -103,10 +105,50 @@ public class MainWindow extends JFrame
     label.setMinimumSize(new Dimension(100, 50));
     label.setMaximumSize(new Dimension(1000, 500));
     logopanel.add(label);
-    
-    
     return logopanel;
   }
   
+  
+  private JPanel FindKundePanel()
+  {
+    JPanel findkundepanel = new JPanel();
+    findkundepanel.setBackground(Color.RED);
+    findkundepanel.setLayout(new BorderLayout(0, 0));
+    
+    JPanel searchpanel = new JPanel();
+    findkundepanel.add(searchpanel, BorderLayout.NORTH);
+    searchpanel.setBackground(Color.RED);
+    JComboBox comboBox = new JComboBox();
+    comboBox.setModel(new DefaultComboBoxModel(new String[] {"Cpr-nummer", "Navn", "Tlf-nr.", "Bil-chassisnummer"}));
+    searchpanel.add(comboBox);
+//    comboBox.actionPerformed(  );
+    searchField = new JTextField();
+    searchpanel.add(searchField);
+    searchField.setColumns(10);
+    JButton searchButton = new JButton("New button");
+    searchpanel.add(searchButton);
+//    searchButton.addActionListener(  );
+
+    JPanel resultpanel = new JPanel();
+    resultpanel.setBackground(Color.RED);
+    findkundepanel.add(resultpanel, BorderLayout.CENTER);
+    
+    JPanel buttonpanel = new JPanel();
+    FlowLayout flowLayout = (FlowLayout) buttonpanel.getLayout();
+    flowLayout.setAlignment(FlowLayout.RIGHT);
+    buttonpanel.setBackground(Color.RED);
+    findkundepanel.add(buttonpanel, BorderLayout.SOUTH);
+    JButton tilbageButton = new JButton("Tilbage");
+    buttonpanel.add(tilbageButton);
+//    tilbageButton.addActionListener(  );
+    JButton redigerButton = new JButton("Rediger");
+    buttonpanel.add(redigerButton);
+//    redigerButton.addActionListener(  );
+    
+    
+    
+    
+    return findkundepanel;
+  }
   
 }
