13089cfc6c7179a206e4c0aa1c42b21884169404
Dennis Lund <Dennis.l.christiansen@gmail.com>
2015-05-11 02:49:59 +0200

    Oprettet dialog-vindue og panel til indtastning/redigering af biloplysninger.

---
+++ 04-Implemetation/Ferrari Finansiering/src/prototypes/EditDialog.java
@@ -0,0 +1,215 @@
+package prototypes;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+
+
+
+public class EditDialog extends JDialog
+{
+  
+  private final JPanel contentPanel = new JPanel();
+  private JTextField chassisnummerField;
+  private JTextField regnummerField;
+  private JTextField prisField;
+  private JTextField modelField;
+  private JTextField årgangField;
+  private JTextField bemærkningField;
+  
+  
+  
+  public EditDialog()
+  {
+    setBackground(Color.RED);
+    setDefaultCloseOperation( JDialog.DISPOSE_ON_CLOSE );
+    setBounds( 100, 100, 450, 380 );
+    contentPanel.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
+    contentPanel.setBackground(Color.RED);
+    setContentPane(contentPanel);
+    contentPanel.setLayout(new BorderLayout(0, 0));
+    
+    
+    JPanel toppanel = LogoPanel();
+    contentPanel.add(toppanel, BorderLayout.NORTH);
+    
+    JPanel bilpanel = BilPanel();
+    contentPanel.add(bilpanel, BorderLayout.CENTER);
+
+    JPanel buttonpanel = ButtonPanel();
+    contentPanel.add(buttonpanel, BorderLayout.SOUTH);
+    
+  }
+  
+  
+  private JPanel ButtonPanel()
+  {
+    JPanel buttonpanel = new JPanel();
+    buttonpanel.setBackground(Color.RED);
+    buttonpanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+    
+    JButton button = new JButton("OK");
+    button.setActionCommand("OK");
+    buttonpanel.add(button);
+    
+    JButton button_1 = new JButton("Cancel");
+    button_1.setActionCommand("Cancel");
+    buttonpanel.add(button_1);
+    
+    
+    return buttonpanel;
+  }
+  
+  
+  private JPanel BilPanel()
+  {
+    JPanel bilpanel = new JPanel();
+    bilpanel.setBackground(Color.RED);
+    
+    GridBagLayout gbl_bilpanel = new GridBagLayout();
+    gbl_bilpanel.columnWidths = new int[]{0, 0, 0};
+    gbl_bilpanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+    gbl_bilpanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+    gbl_bilpanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+    bilpanel.setLayout(gbl_bilpanel);
+    
+    JLabel lblOverskrift = new JLabel("Bil");
+    lblOverskrift.setFont(new Font("Tahoma", Font.PLAIN, 18));
+    GridBagConstraints gbc_lblOverskrift = new GridBagConstraints();
+    gbc_lblOverskrift.insets = new Insets(0, 0, 5, 0);
+    gbc_lblOverskrift.gridx = 1;
+    gbc_lblOverskrift.gridy = 0;
+    bilpanel.add(lblOverskrift, gbc_lblOverskrift);
+    
+    JLabel lblChassisnummer = new JLabel("Chassis-nummer:");
+    GridBagConstraints gbc_lblChassisnummer = new GridBagConstraints();
+    gbc_lblChassisnummer.anchor = GridBagConstraints.EAST;
+    gbc_lblChassisnummer.insets = new Insets(0, 0, 5, 5);
+    gbc_lblChassisnummer.gridx = 0;
+    gbc_lblChassisnummer.gridy = 1;
+    bilpanel.add(lblChassisnummer, gbc_lblChassisnummer);
+    
+    chassisnummerField = new JTextField();
+    GridBagConstraints gbc_chassisnummerField = new GridBagConstraints();
+    gbc_chassisnummerField.insets = new Insets(0, 0, 5, 0);
+    gbc_chassisnummerField.fill = GridBagConstraints.HORIZONTAL;
+    gbc_chassisnummerField.gridx = 1;
+    gbc_chassisnummerField.gridy = 1;
+    bilpanel.add(chassisnummerField, gbc_chassisnummerField);
+    chassisnummerField.setColumns(10);
+    
+    JLabel lblRegNummer = new JLabel("Reg. nummer:");
+    GridBagConstraints gbc_lblRegNummer = new GridBagConstraints();
+    gbc_lblRegNummer.anchor = GridBagConstraints.EAST;
+    gbc_lblRegNummer.insets = new Insets(0, 0, 5, 5);
+    gbc_lblRegNummer.gridx = 0;
+    gbc_lblRegNummer.gridy = 2;
+    bilpanel.add(lblRegNummer, gbc_lblRegNummer);
+    
+    regnummerField = new JTextField();
+    GridBagConstraints gbc_regnummerField = new GridBagConstraints();
+    gbc_regnummerField.insets = new Insets(0, 0, 5, 0);
+    gbc_regnummerField.fill = GridBagConstraints.HORIZONTAL;
+    gbc_regnummerField.gridx = 1;
+    gbc_regnummerField.gridy = 2;
+    bilpanel.add(regnummerField, gbc_regnummerField);
+    regnummerField.setColumns(10);
+    
+    JLabel lblPris = new JLabel("Pris:");
+    GridBagConstraints gbc_lblPris = new GridBagConstraints();
+    gbc_lblPris.anchor = GridBagConstraints.EAST;
+    gbc_lblPris.insets = new Insets(0, 0, 5, 5);
+    gbc_lblPris.gridx = 0;
+    gbc_lblPris.gridy = 3;
+    bilpanel.add(lblPris, gbc_lblPris);
+    
+    prisField = new JTextField();
+    GridBagConstraints gbc_prisField = new GridBagConstraints();
+    gbc_prisField.insets = new Insets(0, 0, 5, 0);
+    gbc_prisField.fill = GridBagConstraints.HORIZONTAL;
+    gbc_prisField.gridx = 1;
+    gbc_prisField.gridy = 3;
+    bilpanel.add(prisField, gbc_prisField);
+    prisField.setColumns(10);
+    
+    JLabel lblModel = new JLabel("Model:");
+    GridBagConstraints gbc_lblModel = new GridBagConstraints();
+    gbc_lblModel.anchor = GridBagConstraints.EAST;
+    gbc_lblModel.insets = new Insets(0, 0, 5, 5);
+    gbc_lblModel.gridx = 0;
+    gbc_lblModel.gridy = 4;
+    bilpanel.add(lblModel, gbc_lblModel);
+    
+    modelField = new JTextField();
+    GridBagConstraints gbc_modelField = new GridBagConstraints();
+    gbc_modelField.insets = new Insets(0, 0, 5, 0);
+    gbc_modelField.fill = GridBagConstraints.HORIZONTAL;
+    gbc_modelField.gridx = 1;
+    gbc_modelField.gridy = 4;
+    bilpanel.add(modelField, gbc_modelField);
+    modelField.setColumns(10);
+    
+    JLabel lblrgang = new JLabel("\u00C5rgang:");
+    GridBagConstraints gbc_lblrgang = new GridBagConstraints();
+    gbc_lblrgang.anchor = GridBagConstraints.EAST;
+    gbc_lblrgang.insets = new Insets(0, 0, 5, 5);
+    gbc_lblrgang.gridx = 0;
+    gbc_lblrgang.gridy = 5;
+    bilpanel.add(lblrgang, gbc_lblrgang);
+    
+    årgangField = new JTextField();
+    GridBagConstraints gbc_årgangField = new GridBagConstraints();
+    gbc_årgangField.insets = new Insets(0, 0, 5, 0);
+    gbc_årgangField.fill = GridBagConstraints.HORIZONTAL;
+    gbc_årgangField.gridx = 1;
+    gbc_årgangField.gridy = 5;
+    bilpanel.add(årgangField, gbc_årgangField);
+    årgangField.setColumns(10);
+    
+    JLabel lblBemrkninger = new JLabel("Bem\u00E6rkninger:");
+    GridBagConstraints gbc_lblBemrkninger = new GridBagConstraints();
+    gbc_lblBemrkninger.anchor = GridBagConstraints.EAST;
+    gbc_lblBemrkninger.insets = new Insets(0, 0, 0, 5);
+    gbc_lblBemrkninger.gridx = 0;
+    gbc_lblBemrkninger.gridy = 6;
+    bilpanel.add(lblBemrkninger, gbc_lblBemrkninger);
+    
+    bemærkningField = new JTextField();
+    GridBagConstraints gbc_bemærkningField = new GridBagConstraints();
+    gbc_bemærkningField.fill = GridBagConstraints.HORIZONTAL;
+    gbc_bemærkningField.gridx = 1;
+    gbc_bemærkningField.gridy = 6;
+    bilpanel.add(bemærkningField, gbc_bemærkningField);
+    bemærkningField.setColumns(10);
+    
+    
+    
+    return bilpanel;
+  }
+  
+  
+  private JPanel LogoPanel()
+  {
+    JPanel logopanel = new JPanel();
+    FlowLayout flowLayout = (FlowLayout) logopanel.getLayout();
+    flowLayout.setAlignment(FlowLayout.LEFT);
+    logopanel.setBackground(Color.RED);
+    JLabel label = new JLabel(new ImageIcon("C:\\Users\\Dennis\\Documents\\GitHub\\1YearProject-Gruppe-3\\04-Implemetation\\Ferrari Finansiering\\Yellow logo on red background.png"));
+    label.setLabelFor(label);
+    label.setSize(new Dimension(300, 100));
+    label.setBackground(new Color(200, 50, 50));
+    label.setIconTextGap(0);
+    label.setPreferredSize(new Dimension(300, 100));
+    label.setMinimumSize(new Dimension(100, 50));
+    label.setMaximumSize(new Dimension(1000, 500));
+    logopanel.add(label);
+    
+    return logopanel;
+  }
+  
+  
+  
+  
+}
--- 04-Implemetation/Ferrari Finansiering/src/prototypes/MainWindow.java
+++ 04-Implemetation/Ferrari Finansiering/src/prototypes/MainWindow.java
@@ -13,13 +13,10 @@ public class MainWindow extends JFrame
 {
   
   private JPanel contentPane;
-  private JTextField searchField;
+  private String comboString[];
   
 
-  
-  /**
-   * Create the frame.
-   */
+
   public MainWindow()
   {
     setBackground(Color.RED);
@@ -34,54 +31,50 @@ public class MainWindow extends JFrame
     JPanel logopanel = LogoPanel();
     contentPane.add(logopanel, BorderLayout.NORTH);
     
-//    JPanel sælgerpanel = SælgerPanel();
-//    contentPane.add(sælgerpanel, BorderLayout.CENTER);
+//    JPanel sælgermainpanel = SælgerMainPanel();
+//    contentPane.add(sælgermainpanel, BorderLayout.CENTER);
     
-    JPanel findkundepanel = FindKundePanel();
-    contentPane.add(findkundepanel, BorderLayout.CENTER);
+//    JPanel findkundepanel = FindKundePanel();
+//    contentPane.add(findkundepanel, BorderLayout.CENTER);
+    
+//    JPanel findbilpanel = FindBilPanel();
+//    contentPane.add(findbilpanel, BorderLayout.CENTER);
+    
+    JPanel findaftalepanel = FindLåneaftalePanel();
+    contentPane.add(findaftalepanel, BorderLayout.CENTER);
 
     
     
   }
   
   
-  
-  private JPanel SælgerPanel()
+  private JPanel SælgerMainPanel()
   {
     JPanel sælgerpanel = new JPanel();
     sælgerpanel.setBackground(Color.RED);
-    
     sælgerpanel.setBorder( new EmptyBorder( 10, 10, 10, 10 ) );
     sælgerpanel.setLayout(new BoxLayout(sælgerpanel, BoxLayout.Y_AXIS));
-    
     JLabel label0 = new JLabel();
     label0.setText(" ");
     sælgerpanel.add( label0 );
-    
     JButton btnOpretLåneanmodning = new JButton("Opret låneanmodning");
     sælgerpanel.add(btnOpretLåneanmodning);
 //    btnOpretLåneanmodning.addActionListener(  );
-    
     JLabel label1 = new JLabel();
     label1.setText(" ");
     sælgerpanel.add( label1 );
-    
     JButton btnFindKunde = new JButton("Find kunde");
     sælgerpanel.add(btnFindKunde);
  //   btnFindKunde.addActionListener(  );
-    
     JLabel label2 = new JLabel();
     label2.setText(" ");
     sælgerpanel.add( label2 );
-    
     JButton btnFindBil = new JButton("Find bil");
     sælgerpanel.add(btnFindBil);
 //    btnFindBil.addActionListener(  );
-    
     JLabel label3 = new JLabel();
     label3.setText(" ");
     sælgerpanel.add( label3 );
-    
     JButton btnFindLåneaftale = new JButton("Find låneaftale");
     sælgerpanel.add(btnFindLåneaftale);
 //    btnFindLåneaftale.addActionListener(  );
@@ -105,6 +98,7 @@ public class MainWindow extends JFrame
     label.setMinimumSize(new Dimension(100, 50));
     label.setMaximumSize(new Dimension(1000, 500));
     logopanel.add(label);
+    
     return logopanel;
   }
   
@@ -115,40 +109,128 @@ public class MainWindow extends JFrame
     findkundepanel.setBackground(Color.RED);
     findkundepanel.setLayout(new BorderLayout(0, 0));
     
-    JPanel searchpanel = new JPanel();
+    String[] kundeString = new String[] {"Cpr-nummer", "Navn", "Tlf-nr.", "Bil-chassisnummer"};
+    setComboString(kundeString);
+    
+    JPanel searchpanel = SearchPanel();
     findkundepanel.add(searchpanel, BorderLayout.NORTH);
-    searchpanel.setBackground(Color.RED);
-    JComboBox comboBox = new JComboBox();
-    comboBox.setModel(new DefaultComboBoxModel(new String[] {"Cpr-nummer", "Navn", "Tlf-nr.", "Bil-chassisnummer"}));
-    searchpanel.add(comboBox);
-//    comboBox.actionPerformed(  );
-    searchField = new JTextField();
-    searchpanel.add(searchField);
-    searchField.setColumns(10);
-    JButton searchButton = new JButton("New button");
-    searchpanel.add(searchButton);
-//    searchButton.addActionListener(  );
-
-    JPanel resultpanel = new JPanel();
-    resultpanel.setBackground(Color.RED);
+    
+    JPanel resultpanel =ResultPanel();
     findkundepanel.add(resultpanel, BorderLayout.CENTER);
     
-    JPanel buttonpanel = new JPanel();
-    FlowLayout flowLayout = (FlowLayout) buttonpanel.getLayout();
-    flowLayout.setAlignment(FlowLayout.RIGHT);
-    buttonpanel.setBackground(Color.RED);
+    JPanel buttonpanel = ButtonPanel();
     findkundepanel.add(buttonpanel, BorderLayout.SOUTH);
-    JButton tilbageButton = new JButton("Tilbage");
-    buttonpanel.add(tilbageButton);
-//    tilbageButton.addActionListener(  );
-    JButton redigerButton = new JButton("Rediger");
-    buttonpanel.add(redigerButton);
-//    redigerButton.addActionListener(  );
-    
-    
-    
-    
+
     return findkundepanel;
   }
+  
+  
+  private JPanel FindBilPanel()
+  {
+    JPanel findbilpanel = new JPanel();
+    
+    findbilpanel.setBackground(Color.RED);
+    findbilpanel.setLayout(new BorderLayout(0, 0));
+    
+    String[] bilString = new String[] {"Chassis-nummer", "Reg. nummer", "Model"};
+    setComboString(bilString);
+    
+    JPanel searchpanel = SearchPanel();
+    findbilpanel.add(searchpanel, BorderLayout.NORTH);
+    
+    JPanel resultpanel =ResultPanel();
+    findbilpanel.add(resultpanel, BorderLayout.CENTER);
+    
+    JPanel buttonpanel = ButtonPanel();
+    findbilpanel.add(buttonpanel, BorderLayout.SOUTH);
+
+    return findbilpanel;
+  }
+  
+  
+  private JPanel FindLåneaftalePanel()
+  {
+    JPanel findaftalepanel = new JPanel();
+    
+    findaftalepanel.setBackground(Color.RED);
+    findaftalepanel.setLayout(new BorderLayout(0, 0));
+    
+    String[] aftaleString = new String[] {"Aftale-ID", "Cpr-ID", "Cpr-nummer", "Chassis-nummer", "Reg.-nummer", "Sælger-ID"};
+    setComboString(aftaleString);
+    
+    JPanel searchpanel = SearchPanel();
+    findaftalepanel.add(searchpanel, BorderLayout.NORTH);
+    
+    JPanel resultpanel =ResultPanel();
+    findaftalepanel.add(resultpanel, BorderLayout.CENTER);
+    
+    JPanel buttonpanel = ButtonPanel();
+    findaftalepanel.add(buttonpanel, BorderLayout.SOUTH);
+
+    return findaftalepanel;
+  }
+  
+  
+                    private JPanel SearchPanel()
+                    {
+                      JPanel searchpanel = new JPanel();
+                      searchpanel.setBackground(Color.RED);
+                      JComboBox comboBox = new JComboBox();
+                      comboBox.setModel(new DefaultComboBoxModel(comboString));
+                      searchpanel.add(comboBox);
+                  //    comboBox.actionPerformed(  );
+                      JTextField searchField = new JTextField();
+                      searchpanel.add(searchField);
+                      searchField.setColumns(10);
+                      JButton searchButton = new JButton("Søg");
+                      searchpanel.add(searchButton);
+                  //    searchButton.addActionListener(  );
+                      
+                      return searchpanel;
+                    }
+                    
+                    
+                    private JPanel ResultPanel()
+                    {
+                      JPanel resultpanel = new JPanel();
+                      resultpanel.setBackground(Color.RED);
+                      
+                      return resultpanel;
+                    }
+                    
+                    
+                    private JPanel ButtonPanel()
+                    {
+                      JPanel buttonpanel = new JPanel();
+                      FlowLayout flowLayout = (FlowLayout) buttonpanel.getLayout();
+                      flowLayout.setAlignment(FlowLayout.RIGHT);
+                      buttonpanel.setBackground(Color.RED);
+                      JButton tilbageButton = new JButton("Tilbage");
+                      buttonpanel.add(tilbageButton);
+                      //  tilbageButton.addActionListener(  );
+                      JButton redigerButton = new JButton("Rediger");
+                      buttonpanel.add(redigerButton);
+                      //  redigerButton.addActionListener(  );
+                      
+                      return buttonpanel;
+                    }
+   
+
+
+
+  
+  
+  
+  
+  public String[] getComboString()
+  {
+    return comboString;
+  }
+
+  public void setComboString( String[] comboString )
+  {
+    this.comboString = comboString;
+  }
+  
   
 }
